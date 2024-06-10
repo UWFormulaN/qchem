a9897dd037ca73d80aa624b7f6373df25850d3b6
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-06-10 01:28:24 -0400

    Saved Bonds to a Data Frame and added a Display Function
    Saved the Bond Graph info to a Data Frame with Variable Bonds
    Created a dedicated function for Displaying the Bond Graph with nice formatting

--- qchem/molecule.py
+++ qchem/molecule.py
@@ -23,6 +23,11 @@ class Molecule:
     XYZCoordinates: pd.core.frame.DataFrame
     """List of the XYZ Coordinates of the Atoms in the Molecule"""
 
+    AtomCount: int = 0
+
+    # Atom Index, Atom Symbol, Bond Index 1,Bond Index 2 , Bond Index 3, Bond Index 4
+    Bonds: pd.core.frame.DataFrame
+
     energy = None
     """The Energy of the Molecule"""
 
@@ -47,14 +52,16 @@ class Molecule:
         for atom in self.XYZCoordinates.itertuples():
             print(atom)
 
-    def GetRadius(self, coord1, coord2):
+    def GetRadius(self, atom1, atom2):
+        """Gets the Radius Between 2 Atoms """
         return sqrt(
-            pow(coord1[0] - coord2[0], 2)
-            + pow(coord1[1] - coord2[1], 2)
-            + pow(coord1[2] - coord2[2], 2)
+            pow(atom1[0] - atom2[0], 2)
+            + pow(atom1[1] - atom2[1], 2)
+            + pow(atom1[2] - atom2[2], 2)
         )
+    
 
-    def GetBondGraph(self):
+    def GetBonds(self):
 
         at_types = self.XYZCoordinates["Atom"].values
         coords = [
@@ -65,11 +72,11 @@ class Molecule:
             )
             for i in range(0, len(self.XYZCoordinates["X"].values))
         ]
-        bond_graphs = [[] for i in range(len(at_types))]
+        bond_graphs = [[] for i in range(self.AtomCount)]
 
-        for i in range(len(at_types)):
+        for i in range(self.AtomCount):
             radii1 = CovalentRadiiConstants[at_types[i]]
-            for j in range(i + 1, len(at_types)):
+            for j in range(i + 1, self.AtomCount):
                 radii2 = CovalentRadiiConstants[at_types[j]]
                 thresh = 1.1 * (radii1 + radii2)
                 dist = self.GetRadius(coords[i], coords[j])
@@ -77,16 +84,44 @@ class Molecule:
                     bond_graphs[i].append(j)
                     bond_graphs[j].append(i)
 
+        index = [i+1 for i in range(self.AtomCount)] 
+        bondDataFrame = pd.DataFrame({
+            "Index": index,
+            "Atom" : at_types,
+            'Bonds': bond_graphs
+        })
+        self.Bonds = bondDataFrame
+
+        
+    def DisplayBondGraph (self):
+
         print("%s\n" % (self.name), end="")
-        for i in range(len(at_types)):
-            print(" %4i %-2s -" % (i + 1, at_types[i]), end="")
-            for j in range(len(bond_graphs[i])):
-                print(" %i" % (bond_graphs[i][j] + 1), end="")
-            print("\n", end="")
-        print("\n", end="")
+
+        for i in range(self.AtomCount):
+
+            index = self.Bonds["Index"][i]
+
+            atom = self.Bonds["Atom"][i]
+
+            # Create the String for Bonds 
+            bonds = ""
+            for i in self.Bonds["Bonds"][i]:
+                bonds += str(i) + " "
+
+            print(" %4i   %-2s - %s" % (index, atom, bonds))
+
+        
+
+
+
+
 
     def __init__(self, name: str, XYZFilePath: str):
         self.name = name
 
         # Load the XYZ File from XYZ File
         self.XYZCoordinates = self.ReadXYZ(path=XYZFilePath)
+
+        self.AtomCount = len(self.XYZCoordinates["Atom"].values)
+
+        self.GetBonds()
