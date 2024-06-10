9869c7eba02c02195067afe5deafdbace7a0fece
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-06-09 23:52:40 -0400

    Successful Molecule import Test from XYZ File

--- qchem/molecule.py
+++ qchem/molecule.py
@@ -1,5 +1,9 @@
+from ast import List
+from os import read
+import string
 import pandas as pd
 from scipy import optimize
+from .Data.constants import CovalentRadiiConstants
 
 # We will create molecule objects which will store information about the molecule
 # Includes coordinates, atom types, how optimization was performed, how energy calculations were performed, etc.
@@ -7,20 +11,40 @@ from scipy import optimize
 
 
 class Molecule:
-    def __init__(
-        self,
-        name,
-        multiplicity,
-        coordinates,
-        atom_types,
-        optimization_method,
-        energy_method,
-        energy,
-    ):
+
+    # The Following are Variables that don't need to be initialized immediately or specified by the user, but belong to the Molecule class
+    # For Some reason the Description needs to be defined after?
+
+    name: str = ""
+    """Name of the Molecule"""
+
+    #  Atom Symbol, X, Y, Z ?
+    XYZCoordinates: pd.core.frame.DataFrame
+    """List of the XYZ Coordinates of the Atoms in the Molecule"""
+
+    energy = None
+    """The Energy of the Molecule"""
+
+    multiplicity = None
+    """Multiplicity of the Molecule"""
+
+    atom_types = None
+    """Atom types of the Molecule"""
+
+    optimization_method = None
+    """Optimization Method of the Molecule"""
+
+    energy_method = None
+    """Energy Method used for the Molecule"""
+
+    def ReadXYZ(self, path: str) -> pd.core.frame.DataFrame:
+        return pd.read_csv(
+            path, sep=r"\s+", skiprows=1, names=["Atom", "X", "Y", "Z"], engine="python"
+        )
+
+    def __init__(self, name: str, XYZFilePath: str):
         self.name = name
-        self.multiplicity = multiplicity
-        self.coordinates = coordinates
-        self.atom_types = atom_types
-        self.optimization_method = optimization_method
-        self.energy_method = energy_method
-        self.energy = energy
+
+        # Load the XYZ File from XYZ File
+        self.XYZCoordinates = self.ReadXYZ(path=XYZFilePath)
+
