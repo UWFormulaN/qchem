72f38283334ec15d5ba72bc46906b76f811c9649
Alexandre Dufresne Nappert <93613553+MrDNAlex@users.noreply.github.com>
2024-06-15 18:46:52 -0400

    Refactor and Trim

--- qchem/molecule.py
+++ qchem/molecule.py
@@ -20,6 +20,7 @@ from .Data.constants import CovalentRadiiConstants
 
 # May need to Redesign all the math to use dependency injection to a Class called Internal Molecular Math
 
+
 class Molecule:
     """Class that represents an Entire Molecule. Stores information aboput Bonds, Atomic Positions unique Atom Properties and Allows for Specific Simulation Calculations"""
 
@@ -27,6 +28,7 @@ class Molecule:
     # For Some reason the Description needs to be defined after?
 
     PositionSlice = slice(1, 4)
+    """Constant that Grabs a Array/List of the Atom Position. Used for the Function GetAtomPosition. Is global so that you can get the position using self.XYZCoordinates.iloc[atomIndex, self.PositionSlice]"""
 
     Name: str = ""
     """Name of the Molecule"""
@@ -70,41 +72,18 @@ class Molecule:
         for atom in self.XYZCoordinates.itertuples():
             print(atom)
 
-    def GetRadius(
-        self, atom1: tuple[float, float, float], atom2: tuple[float, float, float]
-    ):
+    def GetRadius(self, atomIndex1: int, atomIndex2: int):
         """Gets the Radius Between 2 Atoms"""
-        return sqrt(
-            pow(atom1[0] - atom2[0], 2)
-            + pow(atom1[1] - atom2[1], 2)
-            + pow(atom1[2] - atom2[2], 2)
-        )
-    
-    def GetRadiusByIndex(
-        self, atomIndex1: int, atomIndex2: int
-    ):
-        atom1 = self.GetPositionTuple(atomIndex1)
-        atom2 = self.GetPositionTuple(atomIndex2)
-
-        """Gets the Radius Between 2 Atoms"""
-        return sqrt(
-            pow(atom1[0] - atom2[0], 2)
-            + pow(atom1[1] - atom2[1], 2)
-            + pow(atom1[2] - atom2[2], 2)
-        )
+        vector = self.GetAtomPosition(atomIndex1) - self.GetAtomPosition(atomIndex2)
+        return np.linalg.norm(vector)
 
     def GetBonds(self):
         """Generates a Data Frame with all Bond related Information"""
-
         # Pre initialize variables
         at_types = self.XYZCoordinates["Atom"].values
         index = [i for i in range(self.AtomCount)]
         bonds = [[] for i in range(self.AtomCount)]
         bonds_distance = [[] for i in range(self.AtomCount)]
-        coords = [
-            self.GetPositionTuple(i)
-            for i in range(0, len(self.XYZCoordinates["X"].values))
-        ]
 
         # Get the Bonds and save to Array
         for i in range(self.AtomCount):
@@ -112,7 +91,7 @@ class Molecule:
             for j in range(i + 1, self.AtomCount):
                 radii2 = CovalentRadiiConstants[at_types[j]]
                 thresh = 1.1 * (radii1 + radii2)
-                dist = self.GetRadius(coords[i], coords[j])
+                dist = self.GetRadius(i, j)
                 if dist < thresh:
                     bonds[i].append(j)
                     bonds[j].append(i)
@@ -120,7 +99,7 @@ class Molecule:
         # Get Bond Distances
         for i in range(self.AtomCount):
             for j in bonds[i]:
-                bonds_distance[i].append(self.GetRadius(coords[i], coords[j]))
+                bonds_distance[i].append(self.GetRadius(i, j))
 
         # Save new Bonds Data Frame to Bonds Variable
         self.Bonds = pd.DataFrame(
@@ -132,29 +111,12 @@ class Molecule:
             }
         )
 
-    def GetPositionTuple(self, atomIndex):
-        return (
-            self.XYZCoordinates["X"][atomIndex],
-            self.XYZCoordinates["Y"][atomIndex],
-            self.XYZCoordinates["Z"][atomIndex],
-        )
-
-    def GetMagnitude(self, vector):
-        return sqrt( pow(vector[0], 2) + pow(vector[1], 2) + pow(vector[2], 2))
-
-    def GetDotProductAngle(self, vector1, vector2):
-        dotProduct = 0
-        for i in range(len(vector1)):
-            dotProduct += vector1[i] * vector2[i]
-        return math.acos(
-            dotProduct / (self.GetMagnitude(vector1) * self.GetMagnitude(vector2))
-        )
-
     def GetAtomPosition(self, atomIndex):
         """Returns a Numpy Array of the Atoms Position"""
         return np.array(self.XYZCoordinates.iloc[atomIndex, self.PositionSlice], dtype=float)
 
-    def GetDihedralAngle (self, atomIndex1, atomIndex2, atomIndex3, atomIndex4):
+    def GetDihedralAngle(self, atomIndex1, atomIndex2, atomIndex3, atomIndex4):
+        """Returns the Dihedral Angle of between Atoms"""
         atom1Pos = self.GetAtomPosition(atomIndex1)
         atom2Pos = self.GetAtomPosition(atomIndex2)
         atom3Pos = self.GetAtomPosition(atomIndex3)
@@ -174,43 +136,22 @@ class Molecule:
         y = np.dot(m1, v2)
         chi = np.arctan2(y, x)
         chi = -180.0 - 180.0 * chi / np.pi
-        if (chi < -180.0):
+        if chi < -180.0:
             chi = chi + 360.0
-
         return chi
 
-    def GetVector(self, pos1: tuple[float, float, float], pos2: tuple[float, float, float]) -> tuple[float, float, float]: 
-        return (pos2[0] - pos1[0], pos2[1] - pos1[1], pos2[2] - pos1[2])
-
     def GetAngleBetweenAtoms(self, atomIndex1, atomIndex2, atomIndex3):
-        # Atom Index 2 is in the Middle  2 -> 1 and 2 -> 3
-        vec1 = self.GetVector(self.GetPositionTuple(atomIndex2), self.GetPositionTuple(atomIndex1))
-        vec2 = self.GetVector(self.GetPositionTuple(atomIndex2), self.GetPositionTuple(atomIndex3))
-        return (180 / pi) * self.GetDotProductAngle(vec1, vec2)
+        """Returns the Angle between Atoms in Degrees"""
+        atom1Pos = self.GetAtomPosition(atomIndex1)
+        atom2Pos = self.GetAtomPosition(atomIndex2)
+        atom3Pos = self.GetAtomPosition(atomIndex3)
 
-    def PrintAngles(self):
-        angles = []
-        skipList = []
-        for j in range(self.AtomCount):
-            numOfBonds = len(self.Bonds["Bonds"][j])
-            for a in range(numOfBonds):
-                
-                if (len(angles) > 0):
-                    if skipList.__contains__(angles[:][0]): 
-                        next()
-                atomIndex = self.Bonds["Bonds"][j][a]
-                # i = bond_graph[j][a]
-                for b in range(a + 1, numOfBonds):
-                    atomIndex2 = self.Bonds["Bonds"][j][b]
-                    skipList.append(atomIndex2)
-                    # k = bond_graph[j][b]
-                    bondAngle = self.GetAngleBetweenAtoms(atomIndex, j, atomIndex2)
-                    #a123 = get_a123(coords[i], coords[j], coords[k])
-                    angles.append([atomIndex, j, atomIndex2, bondAngle])
+        v1 = atom1Pos - atom2Pos
+        v1 = v1 / np.linalg.norm(v1)
+        v2 = atom3Pos - atom2Pos
+        v2 = v2 / np.linalg.norm(v2)
 
-        for i in range(len(angles)):
-            print(angles[i])
-        return angles
+        return (180 / pi) * math.acos(np.dot(v1, v2))
 
     def DisplayBondGraph(self):
         """Displays the Bond Graph in Terminal"""
@@ -235,32 +176,17 @@ class Molecule:
             # Print Line to Screen
             print(" %4i   %-2s - %s    %4s" % (index + 1, atom, bonds, bond_dist))
 
-    def RecursiveBondSearch (self, depth, atomIndex, previousVisits: list[int],  previousChain: tuple[int, int, int]):
-        for i in self.Bonds["Bonds"][atomIndex]:
-            if i in previousVisits:
-                continue
-            if depth >= self.AtomCount:
-                return previousChain
-           
-            newChain = (previousChain[1], previousChain[2], i+1)
-            previousVisits.append(i)
-            print(newChain)
-            result = self.RecursiveBondSearch( depth + 1, i, previousVisits, newChain )
-
-            if result:
-                return result
-
-    def GetAtomChain(self, atomChain: list[int], depth = 0):
-
+    def GetAtomChain(self, atomChain: list[int], depth=0):
+        """Recursively Searches for a Viable Atom Chain of length 4"""
         # Grab Bonds and Sort by incrementing index
-        bonds : list[int] = self.Bonds["Bonds"][atomChain[depth]]
+        bonds: list[int] = self.Bonds["Bonds"][atomChain[depth]]
         bonds.sort()
 
         # Loop through the Sorted Bonds
         for i in bonds:
 
             # Return the Result if we are already at a length of 4
-            if (len(atomChain) == 4):
+            if len(atomChain) == 4:
                 continue
 
             # Skip if Atom is already in chain
@@ -268,10 +194,10 @@ class Molecule:
                 continue
 
             # Get this Atoms Bonds
-            currentAtomBonds : list[int] = self.Bonds["Bonds"][i]
+            currentAtomBonds: list[int] = self.Bonds["Bonds"][i]
 
             # Skip if the Number of Bonds that the Current Atom has is Only one, Unless it is going to be the Last Atom in the Chain
-            if (len(currentAtomBonds) == 1 and (not len(atomChain) == 3)):
+            if len(currentAtomBonds) == 1 and (not len(atomChain) == 3):
                 continue
 
             # Chosen to be a valid chain so we add it to the list
@@ -283,13 +209,13 @@ class Molecule:
         return atomChain
 
     def DisplayZMatrix(self):
+        """Displays the Z Matrix in the Terminal"""
         z_matrix = self.CreateZMatrixFile()
         for i in z_matrix:
             print(i)
 
-    def CreateZMatrixFile (self):
+    def CreateZMatrixFile(self):
         """Creates an Array of Strings that Reprensents the ZMatrix File"""
-
         # Create an Array Reprensenting the File, First line is Number of Atoms, Second is the Name of the Molecule
         z_matrix = []
         z_matrix.append(f"{self.AtomCount}")
@@ -302,37 +228,43 @@ class Molecule:
             atomChain = self.GetAtomChain([i])
 
             # If the Chain hasn't reached 4 add Random Atoms until we reach 4
-            while (len(atomChain) < 4):
+            while len(atomChain) < 4:
                 randomAtom = random.randint(0, self.AtomCount - 1)
                 if randomAtom not in atomChain:
                     atomChain.append(randomAtom)
 
+            # Save the Indexes to Variables
             j = atomChain[1]
             k = atomChain[2]
             l = atomChain[3]
 
+            # Cache the Z Matrix Variables
             atomSymbol = self.XYZCoordinates["Atom"][i]
+            radius = self.GetRadius(i, j)
+            angle = self.GetAngleBetweenAtoms(i, j, k)
+            dihedralAngle = self.GetDihedralAngle(i, j, k, l)
 
             # Check if First Atom, Only Append symbol
-            if (i == 0):
+            if i == 0:
                 z_matrix.append(f"{atomSymbol}")
                 continue
 
             # Check if Second Atom, Append Symbol and Distance from Next Closest Atom
-            if (i == 1):
-                z_matrix.append(f"{atomSymbol} {j+1} {self.GetRadiusByIndex(i, j)}")
+            if i == 1:
+                z_matrix.append(f"{atomSymbol} {j+1} {radius}")
                 continue
 
             # Check if Third Atom, Append Symbol, Distance and Angle
-            if (i == 2):
-                z_matrix.append(f"{atomSymbol} {j+1} {self.GetRadiusByIndex(i, j)} {k+1} {self.GetAngleBetweenAtoms(i, j, k)}")
+            if i == 2:
+                z_matrix.append(f"{atomSymbol} {j+1} {radius} {k+1} {angle}")
                 continue
 
             # Append Symbol, Distance, Angle and Dihedral Angle
-            z_matrix.append(f"{atomSymbol} {j+1} {self.GetRadiusByIndex(i, j)} {k+1} {self.GetAngleBetweenAtoms(i, j, k)} {l+1} {self.GetDihedralAngle(i, j, k, l)}")
+            z_matrix.append(
+                f"{atomSymbol} {j+1} {radius} {k+1} {angle} {l+1} {dihedralAngle}"
+            )
 
         return z_matrix
-    
 
     def __init__(self, name: str, XYZFilePath: str):
         """Initializes a New Molecule Object"""
