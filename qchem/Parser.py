8c44626ad6fca409285b783e9029ca7ae4e077b3
Cameron Chin <cjchin@uwaterloo.ca>
2024-10-16 21:18:20 -0400

    restructured output file parser

--- qchem/Parser.py
+++ qchem/Parser.py
@@ -1,100 +1,79 @@
-# This is top priority for now, get dataframes out of Orca output files for info.
-
 import pandas as pd
 import re
 
-# This section is for parsing geometry files
-# It should include xyz, smiles, mol, etc.
- 
-def read_xyz(path:str) -> pd.core.frame.DataFrame:
-    return pd.read_csv(
-        path, sep=r"\s+", skiprows=1, names=["Atom", "X", "Y", "Z"], engine="python"
-    )
-
-# This section is for parsing specific results from output files
-# So far, it contains end timings, SCF Energy, loedwin and Mayer Analysis
-
-# Add class "Project" or "OrcaProject" that takes a folder, extracts all the .out files and makes them into OrcaOutput objects
-# Will also need a class variable ".name" for OrcaOutput that will be the file name
 
 class OrcaOutput:
-    def __init__(
-        self,
-        file_path
-    ):
+    def __init__(self, file_path):
+        """Initialize the OrcaOutput class with the path to the ORCA output file.
+        Reads the file content and processes different properties such as final timings,
+        Mayer population analysis, Loewdin charges, dipole moments, etc."""
         self.file_path = file_path
-        with open(self.file_path, 'r') as file:
+        with open(self.file_path, "r") as file:
             self.lines = file.readlines()
+        # Call methods to extract relevant data from the file
         self.final_timings = self.read_final_timings()
         self.mayer_population = self.read_mayer()
         self.loedwin = self.read_Loedwin()
         self.dipole = self.read_dipole()
         self.absolutedipole = self.read_absolute_dipole()
-        #self.quadrupole = self.read_quadrupole()
-        self.name = re.search(r"[^\\]+$",self.file_path).group()[:-4]
         self.energy = self.finalenergy()
+        # Extract the name of the file without the path and extension
+        self.name = re.search(r"[^\\]+$", self.file_path).group()[:-4]
 
-    def read_final_timings(self) -> pd.core.frame.DataFrame:   
+    def read_xyz(self, path: str) -> pd.DataFrame:
+        """Reads XYZ format file to extract atomic coordinates and returns it as a DataFrame."""
+        return pd.read_csv(path, sep=r"\s+", skiprows=1, names=["Atom", "X", "Y", "Z"], engine="python")
+
+    def read_final_timings(self) -> pd.DataFrame:
+        """Extracts the final timings from the ORCA output file.
+        These timings give the time spent on various computational modules."""
         for i, line in enumerate(self.lines[-20:]):
-            if line.strip() == 'Timings for individual modules:':
+            if line.strip() == "Timings for individual modules:":
                 start_index = i + 2
                 time_lines = self.lines[-20:][start_index:-2]
 
-                df = pd.DataFrame([line.split('...') for line in time_lines], columns=['Timing', 'Time'])
-                #df = df.apply(pd.to_numeric, errors='coerce').dropna()
-                df[['Time', 'B']] = df['Time'].str.split('sec', n=1, expand=True)
-                return df.drop(['B'],axis=1)
+                # Process timing data into a DataFrame
+                df = pd.DataFrame([line.split("...") for line in time_lines], columns=["Timing", "Time"])
+                df[["Time", "B"]] = df["Time"].str.split("sec", n=1, expand=True)
+                return df.drop(["B"], axis=1)
+
     def finalenergy(self) -> float:
+        """Extracts the final single-point energy from the ORCA output file."""
         for line in self.lines:
             if line.strip()[0:25] == "FINAL SINGLE POINT ENERGY":
                 return float(line.split()[-1])
 
     def read_mayer(self) -> list:
-        # Reads out Mayer Population Analysis data from an Orca ouput file
-
-        #  NA   - Mulliken gross atomic population
-        #  ZA   - Total nuclear charge
-        #  QA   - Mulliken gross atomic charge
-        #  VA   - Mayer's total valence
-        #  BVA  - Mayer's bonded valence
-        #  FA   - Mayer's free valence
-
-        # Returns data of the form [df1, df2, ...] where df1, df2, ... are dataframes for each Mayer Population Analysis section
-        # (typically start and end of geometry optimization)
-
+        """Extracts Mayer Population Analysis data, which provides details on atomic populations,
+        charges, and valence. It returns a list of DataFrames."""
         for line in self.lines:
             if line.strip()[0:15] == "Number of atoms":
                 atom_count = int(line.split()[-1])
 
         mayer_populations = []
-
         for i, line in enumerate(self.lines):
-            if (
-                line.strip()
-                == "ATOM       NA         ZA         QA         VA         BVA        FA"
-            ):
+            if line.strip() == "ATOM       NA         ZA         QA         VA         BVA        FA":
                 start_index = i + 1
                 end_index = i + atom_count + 1
 
                 mayer_lines = self.lines[start_index:end_index]
 
+                # Process Mayer population data into a DataFrame
                 df = pd.DataFrame(
                     [line.split()[1:8] for line in mayer_lines],
                     columns=["ATOM", "NA", "ZA", "QA", "VA", "BVA", "FA"],
                 )
-
-                df[["NA", "ZA", "QA", "VA", "BVA", "FA"]] = df[
-                    ["NA", "ZA", "QA", "VA", "BVA", "FA"]
-                ].astype(float)
+                df[["NA", "ZA", "QA", "VA", "BVA", "FA"]] = df[["NA", "ZA", "QA", "VA", "BVA", "FA"]].astype(float)
 
                 mayer_populations.append(df)
 
         return mayer_populations
 
     def read_Loedwin(self) -> list:
-    # Reads out Loedwin Atomic Charges data
+        """Extracts Loewdin atomic charges from the ORCA output file.
+        Returns a list of DataFrames containing the atomic charges."""
         for line in self.lines:
-            if line.strip()[0:15] == 'Number of atoms':
+            if line.strip()[0:15] == "Number of atoms":
                 atom_count = int(line.split()[-1])
 
         loedwin = []
@@ -102,228 +81,125 @@ class OrcaOutput:
         end_switch = False
 
         for i, line in enumerate(self.lines):
-            if line.strip() == 'LOEWDIN ATOMIC CHARGES':
+            if line.strip() == "LOEWDIN ATOMIC CHARGES":
                 start_index = i + 2
                 start_switch = True
-            if line.strip() == 'LOEWDIN REDUCED ORBITAL CHARGES':
-                end_index = i-2
+            if line.strip() == "LOEWDIN REDUCED ORBITAL CHARGES":
+                end_index = i - 2
                 end_switch = True
             if (start_switch & end_switch) == True:
                 loedwin_lines = self.lines[start_index:end_index]
 
-                df = pd.DataFrame([line.split()[0:5] for line in loedwin_lines], columns=['Count', 'Atom', ':', 'Atomic Charge'])
-                df = df.drop([':'],axis=1)
+                # Process Loewdin charges into a DataFrame
+                df = pd.DataFrame(
+                    [line.split()[0:5] for line in loedwin_lines], columns=["Count", "Atom", ":", "Atomic Charge"]
+                )
+                df = df.drop([":"], axis=1)
                 loedwin.append(df)
                 start_switch = False
                 end_switch = False
 
         return loedwin
-    
+
     def read_SCF_Energy(self) -> list:
-    # Reads out Energy steps for SCF Iterations
-
-    # ITER    - Step number
-    # Energy  - Energy
-    # Delta-E - Change in energy from previous step
-
-    # Returns data of the form [df1, df2, ...] where df1, df2, ... are dataframes for each SCF Iterations section
-    # (which occur at every single point energy calculation)
+        """Extracts the energy values from SCF iterations during the self-consistent field process.
+        Returns a list of DataFrames containing iteration data such as step, energy, and energy change."""
         scf_energies = []
         start_switch = False
         end_switch = False
-    
+
         for i, line in enumerate(self.lines):
-            if line.strip() == 'SCF ITERATIONS':
+            if line.strip() == "SCF ITERATIONS":
                 start_index = i + 1
                 start_switch = True
-            if line.strip() == '***Rediagonalizing the Fockian in SOSCF/NRSCF***':
+            if line.strip() == "***Rediagonalizing the Fockian in SOSCF/NRSCF***":
                 end_index = i + 1
                 end_switch = True
             if (start_switch & end_switch) == True:
                 SCF_lines = self.lines[start_index:end_index]
 
-                df = pd.DataFrame([line.split()[0:3] for line in SCF_lines], columns=['ITER', 'Energy', 'Delta-E'])
-                # There is more data in here, but I don't understand the difference of before and after "Removing any level shift"
-                df = df.apply(pd.to_numeric, errors='coerce').dropna()
+                # Process SCF energy data into a DataFrame
+                df = pd.DataFrame([line.split()[0:3] for line in SCF_lines], columns=["ITER", "Energy", "Delta-E"])
+                df = df.apply(pd.to_numeric, errors="coerce").dropna()
                 scf_energies.append(df)
 
-                # This is a little silly, but it works lol
                 start_switch = False
                 end_switch = False
 
-        return(scf_energies)
-    
+        return scf_energies
+
     def read_dipole(self) -> tuple:
+        """Extracts the total dipole moment (vector) from the ORCA output file."""
         for line in self.lines:
             if line.strip()[0:19] == "Total Dipole Moment":
                 return tuple(map(float, line.split()[4:]))
+
     def read_absolute_dipole(self) -> float:
+        """Extracts the magnitude of the dipole moment from the ORCA output file."""
         for line in self.lines:
             if line.strip()[0:15] == "Magnitude (a.u.)":
-                return line.split()[2]
-            
-    def read_quadrupole(self) -> tuple:
+                return float(line.split()[2])
+
+    def read_gibbs(self) -> tuple:
+        """Extracts the Gibbs free energy from the ORCA output file, returning a tuple of the energy and its unit."""
         for i, line in enumerate(self.lines):
-            if line.strip()[0:24] == "QUADRUPOLE MOMENT (A.U.)":
-                df = pd.read_csv(self.lines[i+3:i+7], delim_whitespace=True, names=["Type", "XX", "YY", "ZZ", "XY", "XZ", "YZ"])
-                return df
-            
+            if line.strip()[0:23] == "Final Gibbs free energy":
+                gibbs = float(re.search(r"-?\d+\.\d+", line).group())
+                unit = re.search(r"\b\w+\b$", line).group()
+                return gibbs, unit
 
-def read_final_timings(output_path: str) -> pd.core.frame.DataFrame:
-    with open(output_path, 'r') as file:
-        lines = file.readlines()
-    
-    for i, line in enumerate(lines[-20:]):
-        if line.strip() == 'Timings for individual modules:':
-            start_index = i + 2
-            time_lines = lines[-20:][start_index:-2]
+    def find_partition(self, gibbs_1: tuple, gibbs_2: tuple, temp=293.15) -> float:
+        """Calculates the partition coefficient between two Gibbs free energies at a given temperature."""
+        delta_gibbs = gibbs_1[0] * self.convert_energy(gibbs_1[1], "Eh") - gibbs_2[0] * self.convert_energy(gibbs_2[1], "Eh")
+        delta_gibbs = delta_gibbs * self.convert_energy("Eh", "J/mol")
+        return -delta_gibbs / (8.314 * temp)
 
-            df = pd.DataFrame([line.split('...') for line in time_lines], columns=['Timing', 'Time'])
-            #df = df.apply(pd.to_numeric, errors='coerce').dropna()
-            df[['Time', 'B']] = df['Time'].str.split('sec', n=1, expand=True)
-            df = df.drop(['B'],axis=1)
-            return df
+    def convert_energy(self, unit1: str, unit2: str) -> float:
+        """Converts energy values between different units such as Eh, eV, kJ/mol, kcal/mol, and J/mol."""
+        energy = {"Eh": 1, "eV": 27.2107, "kJ/mol": 2625.5, "kcal/mol": 627.503, "J/mol": 2625500}
+        return energy[unit2] / energy[unit1]
 
-def read_SCF_Energy(output_path: str) -> list:
-# Reads out Energy steps for SCF Iterations
+    def relative_polarity(self):
+        """Placeholder for a function to calculate relative polarity."""
+        return
 
-# ITER    - Step number
-# Energy  - Energy
-# Delta-E - Change in energy from previous step
+    def save_to_txt(self, output_path: str):
+        """Writes all the extracted information to a text file in a human-readable format."""
+        with open(output_path, "w") as file:
+            file.write(f"File: {self.file_path}\n")
+            file.write(f"Final Single Point Energy: {self.energy}\n\n")
 
-# Returns data of the form [df1, df2, ...] where df1, df2, ... are dataframes for each SCF Iterations section
-# (which occur at every single point energy calculation)
-    with open(output_path, 'r') as file:
-        lines = file.readlines()
+            file.write("SCF Energies:\n")
+            for df in self.read_SCF_Energy():
+                file.write(df.to_string(index=False))
+                file.write("\n\n")
 
-    scf_energies = []
-    start_switch = False
-    end_switch = False
-    
-    for i, line in enumerate(lines):
-        if line.strip() == 'SCF ITERATIONS':
-            start_index = i + 1
-            start_switch = True
-        if line.strip() == '***Rediagonalizing the Fockian in SOSCF/NRSCF***':
-            end_index = i + 1
-            end_switch = True
-        if (start_switch & end_switch) == True:
-            SCF_lines = lines[start_index:end_index]
+            file.write("Final Timings:\n")
+            if isinstance(self.final_timings, pd.DataFrame):
+                file.write(self.final_timings.to_string(index=False))
+                file.write("\n\n")
 
-            df = pd.DataFrame([line.split()[0:3] for line in SCF_lines], columns=['ITER', 'Energy', 'Delta-E'])
-            # There is more data in here, but I don't understand the difference of before and after "Removing any level shift"
-            df = df.apply(pd.to_numeric, errors='coerce').dropna()
-            scf_energies.append(df)
+            file.write("Mayer Population Analysis:\n")
+            for i, df in enumerate(self.mayer_population):
+                file.write(f"Step {i + 1}:\n")
+                file.write(df.to_string(index=False))
+                file.write("\n\n")
 
-            # This is a little silly, but it works lol
-            start_switch = False
-            end_switch = False
+            file.write("Loewdin Charges:\n")
+            for i, df in enumerate(self.loedwin):
+                file.write(f"Step {i + 1}:\n")
+                file.write(df.to_string(index=False))
+                file.write("\n\n")
 
-    return(scf_energies)
+            file.write(f"Total Dipole Moment: {self.dipole}\n")
+            file.write(f"Magnitude of Dipole Moment: {self.absolutedipole}\n\n")
 
-def read_mayer(output_path: str) -> list:
-    # Reads out Mayer Population Analysis data from an Orca ouput file
-
-    #  NA   - Mulliken gross atomic population
-    #  ZA   - Total nuclear charge
-    #  QA   - Mulliken gross atomic charge
-    #  VA   - Mayer's total valence
-    #  BVA  - Mayer's bonded valence
-    #  FA   - Mayer's free valence
-
-    # Returns data of the form [df1, df2, ...] where df1, df2, ... are dataframes for each Mayer Population Analysis section
-    # (typically start and end of geometry optimization)
-    with open(output_path, "r") as file:
-        lines = file.readlines()
-
-    for line in lines:
-        if line.strip()[0:15] == "Number of atoms":
-            atom_count = int(line.split()[-1])
-
-    mayer_populations = []
-
-    for i, line in enumerate(lines):
-        if (
-            line.strip()
-            == "ATOM       NA         ZA         QA         VA         BVA        FA"
-        ):
-            start_index = i + 1
-            end_index = i + atom_count + 1
-
-            mayer_lines = lines[start_index:end_index]
-
-            df = pd.DataFrame(
-                [line.split()[1:8] for line in mayer_lines],
-                columns=["ATOM", "NA", "ZA", "QA", "VA", "BVA", "FA"],
-            )
-
-            df[["NA", "ZA", "QA", "VA", "BVA", "FA"]] = df[
-                ["NA", "ZA", "QA", "VA", "BVA", "FA"]
-            ].astype(float)
-
-            mayer_populations.append(df)
-
-    return mayer_populations
-
-def read_Loedwin(output_path: str) -> list:
-# Reads out Energy steps for SCF Iterations
-
-# ITER    - Step number
-# Energy  - Energy
-# Delta-E - Change in energy from previous step
-
-# Returns data of the form [df1, df2, ...] where df1, df2, ... are dataframes for each SCF Iterations section
-# (which occur at every single point energy calculation)
-    with open(output_path, 'r') as file:
-        lines = file.readlines()
-
-    for line in lines:
-        if line.strip()[0:15] == 'Number of atoms':
-            atom_count = int(line.split()[-1])
-
-    loedwin = []
-    start_switch = False
-    end_switch = False
-    
-    for i, line in enumerate(lines):
-        if line.strip() == 'LOEWDIN ATOMIC CHARGES':
-            start_index = i + 2
-            start_switch = True
-        if line.strip() == 'LOEWDIN REDUCED ORBITAL CHARGES':
-            end_index = i-2
-            end_switch = True
-        if (start_switch & end_switch) == True:
-            loedwin_lines = lines[start_index:end_index]
-
-            df = pd.DataFrame([line.split()[0:5] for line in loedwin_lines], columns=['Count', 'Atom', ':', 'Atomic Charge'])
-            df = df.drop([':'],axis=1)
-            loedwin.append(df)
-            start_switch = False
-            end_switch = False
-
-    return(loedwin)
+            file.write("Gibbs Free Energy:\n")
+            gibbs = self.read_gibbs()
+            if gibbs:
+                file.write(f"Final Gibbs Free Energy: {gibbs[0]} {gibbs[1]}\n")
 
 
-# This Subsection is for examining gibbs free energy data and calculating solvent partition coefficients
-
-def read_gibbs(output_path: str) -> tuple:
-    with open(output_path, 'r') as file:
-        lines = file.readlines()
-    
-    for i, line in enumerate(lines):
-        if line.strip()[0:23] == 'Final Gibbs free energy':
-            gibbs = float(re.search(r'-?\d+\.\d+', line).group())
-            unit = re.search(r'\b\w+\b$',line).group()
-            return gibbs, unit
-        
-def find_partition(gibbs_1:tuple, gibbs_2:tuple, temp=293.15) -> float:
-    delta_gibbs = gibbs_1[0]*convert_energy(gibbs_1[1],'Eh') - gibbs_2[0]*convert_energy(gibbs_2[1],'Eh')
-    delta_gibbs = delta_gibbs*convert_energy('Eh','J/mol')
-    return -delta_gibbs/(8.314*temp)
-    
-def convert_energy(unit1: str, unit2: str) -> float:
-    energy = {'Eh': 1, 'eV': 27.2107, 'kJ/mol': 2625.5, 'kcal/mol': 627.503, 'J/mol':2625500}
-    return energy[unit2]/energy[unit1]
-
-def relative_polarity():
-    return
+# Example usage
+# orca_output = OrcaOutput(r"C:\path\to\your\ORCA\output\file.out")
+# orca_output.save_to_txt(r"C:\path\to\your\desired\output\file.txt")
