3e5b1996315102f8e6dcbd44c4fea17f4c517550
Alexandre Dufresne Nappert <93613553+MrDNAlex@users.noreply.github.com>
2025-01-19 16:00:49 -0500

    Reformat + getInputFileName

--- qchem/Calculation/BaseOrcaCalculation.py
+++ qchem/Calculation/BaseOrcaCalculation.py
@@ -6,79 +6,89 @@ from ..Molecule import Molecule
 from abc import ABC, abstractmethod
 
 class BaseOrcaCalculation(ABC):
-    
+
     molecule: Molecule | str
     """String Path to the XYZ file or Molecule Object of the Molecule to run a Calculation on"""
-    
+
     template: str | OrcaInputTemplate
     """Template for the Input File that gets filled out to run the Calculation"""
-    
+
     calculationType: str = ""
     """The Keyword for the Calculation to run on the Molecule (For Pipelines replace with name)"""
-    
+
     inputFile: OrcaInputFile
     """The auto generated Input File that is used for the Calculation"""
-    
+
     inputFilePath: str
     """The path the Input File will be saved to, often orcaCachePath + inputFileName.inp"""
 
     outputFilePath: str
-    """The Path to the Output File on the Device"""
-    
-    variables: dict[str, Any]
-    """Additional Variables to be Passed Through to the Input File"""
-    
-    index: int
-    """Container Index so that they can be run in parallel"""
+    """The path the Output File will be saved to, often orcaCachePath + outputFileName.out"""
 
-    calculationName: str
-    """Name of the Calculation. Saves the Input and Output File as the Name"""
+    variables: dict[str, Any]
+    """Additional Variables that will be pasted into the Input File Template"""
+
+    index: int
+    """Number to identify individual Docker Orca Calculations running in parallel"""
+
+    name: str
+    """Friendly name for the calculation, used for Input and Output file names"""
 
     orcaCachePath: str
-    """The Path to the Orca Cache on the Local Device"""
+    """The path to the folder that stores temporary and resulting Calculation Files"""
 
     calculationTime: float
-    """The Time it took to run the Calculation (In Seconds)"""
-    
+    """The resulting total time for the calculation in seconds."""
+
     STDOut: bool
-    """Boolean Flag Determining if Standard Output Messages will be displayed"""
-    
+    """A Boolean flag to indicate if Standard Output logs should be printed"""
+
     isLocal: bool
-    """Boolean Flag determining if the Calculation should be run locally or inside a Container"""
-    
-    cores : int
-    """Number of Cores to use for the Calculation"""
-    
+    """A Boolean flag to indicate if the calculation runs locally or in Docker (True = Local, False = Docker)"""
+
+    cores: int
+    """Number of CPU Cores allocated to the calculation"""
+
     defaultName: str = "Molecule"
-    """Default Name Used to Name a Calculation if not specified. self.name will have the Calculation Type added before 'Molecule' seperated by '_'"""
-    
-    def __init__ (self, name: str, molecule: str | Molecule, template: str | OrcaInputTemplate, index: int, cores: int, isLocal: bool, stdout : bool, **variables):
-        
-         # Value Type Checking
-        if (not (self.calculationType and isinstance(self.calculationType, (str)))):
+    """Default Calculation Name to use if unspecified. Will check if Molecule Object already has a name first."""
+
+    def __init__(
+        self,
+        name: str,
+        molecule: str | Molecule,
+        template: str | OrcaInputTemplate,
+        index: int,
+        cores: int,
+        isLocal: bool,
+        stdout: bool,
+        **variables,
+    ):
+
+        # Value Type Checking
+        if not (self.calculationType and isinstance(self.calculationType, (str))):
             raise ValueError("Calculation Type must be defined and a String")
-         
-        if (not isinstance(name, (str)) or name == ""):
+
+        if not isinstance(name, (str)) or name == "":
             raise ValueError("Name of the Calculation must be specified ")
-        
-        if (not (molecule and isinstance(molecule, (str, Molecule)))):
+
+        if not (molecule and isinstance(molecule, (str, Molecule))):
             raise ValueError("Molecule must be a Molecule Object or String")
-        
-        if (not isinstance(template, (str, OrcaInputTemplate))):
+
+        if not isinstance(template, (str, OrcaInputTemplate)):
             raise ValueError("Template must be a String or OrcaInputTemplate")
-        
-        if (not isinstance(index, (int))):
+
+        if not isinstance(index, (int)):
             raise ValueError("Index must be an integer")
-        
-        if (not isinstance(cores, (int))):
+
+        if not isinstance(cores, (int)):
             raise ValueError("Cores must be an integer")
-        
-        if (not isinstance(isLocal, (bool))):
+
+        if not isinstance(isLocal, (bool)):
             raise ValueError("IsLocal must be a boolean")
-        
-        if (not isinstance(stdout, (bool))):
+
+        if not isinstance(stdout, (bool)):
             raise ValueError("STDOut must be a boolean")
-        
+
         # Set the Values
         self.name = name
         self.molecule = molecule
@@ -88,72 +98,84 @@ class BaseOrcaCalculation(ABC):
         self.isLocal = isLocal
         self.STDOut = stdout
         self.variables = variables
-        
+
         # Set Name to the Name of the Molecule
-        if (self.name == self.defaultName):
-            if (isinstance(molecule, Molecule)):
+        if self.name == self.defaultName:
+            if isinstance(molecule, Molecule):
                 self.name = molecule.Name
             else:
                 self.name = (self.calculationType + " Molecule").replace(" ", "_")
-        
+
         # Set Appropriate XYZ Format and Default Templates (Only if unspecified)
-        if (self.isFileReference()):
+        if self.isFileReference():
             self.variables["xyzfile"] = self.molecule
-            if (self.template == ""):
-                if (self.cores == 1):
+            if self.template == "":
+                if self.cores == 1:
                     self.template = OrcaInputTemplate.BASIC
                 else:
                     self.template = OrcaInputTemplate.BASICPARALLEL
         else:
             self.variables["xyz"] = self.molecule.XYZBody()
-            if (self.template == ""):
-                if (self.cores == 1):
+            if self.template == "":
+                if self.cores == 1:
                     self.template = OrcaInputTemplate.BASICXYZ
                 else:
                     self.template = OrcaInputTemplate.BASICXYZPARALLEL
-        
+
         # Define the Calculation Type and the Number of Cores to Use
         self.variables["calculation"] = self.calculationType
         self.variables["cores"] = self.cores
-            
+
         # Generate Cache Paths
-        orcaCache = "OrcaCache" #Change this to QChemCache?
-        self.orcaCachePath =  os.path.join(os.getcwd(), orcaCache, self.name)
-        
+        orcaCache = "OrcaCache"  # Change this to QChemCache?
+        self.orcaCachePath = os.path.join(os.getcwd(), orcaCache, self.name)
+
         # Convert to Functions?
         self.outputFilePath = os.path.join(self.orcaCachePath, self.getOutputFileName())
         self.inputFilePath = os.path.join(self.orcaCachePath, self.getInputFileName())
 
         # Create the Input File
         self.inputFile = OrcaInputFile(self.template, **self.variables)
-    
+
     @abstractmethod
     def runCalculation(self):
-        """Runs the Orca Calculation"""
+        """Runs the Calculation Algorithm, is an Abstract Method/Function that needs to be overridden.
+
+        Parameters:\n
+            self - Default Parameter for the Class Instance
+
+        Returns: None - No Return Value
+        """
         pass
-    
+
     def isFileReference(self):
-        """Determines if the Molecule is stored as a File Reference, or is a direct Molecule Object"""
-        if (isinstance(self.molecule, (str))):
+        """Checks if the Molecule is defined as a File Reference as a str or a Molecule Object
+
+        Parameters : \n
+            self - Default Parameter for the Class Instance
+
+        Returns : bool - True if the Molecule is a File Reference, False if it is a Molecule Object
+        """
+        if isinstance(self.molecule, (str)):
             return True
         else:
             return False
-        
-    def getInputFileName (self):
+
+    def getInputFileName(self):
         """Returns the Input File Name with it's extension"""
         return f"{self.name}.inp"
 
-    def getOutputFileName (self):
+    def getOutputFileName(self):
         """Returns the Output File Name with it's extension"""
         return f"{self.name}.out"
-    
-    def getOutput (self) -> str:
+
+    def getOutput(self) -> str:
         """Returns the Entire Content of the Output File as a Single String"""
         # Open the Output File and Grab the Content
-        with open(self.outputFilePath, 'r') as file:
+        with open(self.outputFilePath, "r") as file:
             self.CalculationOutput = file.read()
-            
-    def createDirectories (self):
+
+    def createDirectories(self):
         """Creates the necessary Cache Folders to store the Calculation"""
         # Make Cache Folder if it doesn't Exist
         if not os.path.exists(self.orcaCachePath):
@@ -162,7 +184,7 @@ class BaseOrcaCalculation(ABC):
         # Make a folder for the Specific Calculation
         if not os.path.exists(self.orcaCachePath):
             os.makedirs(self.orcaCachePath)
-    
+
     def clockTime(self, seconds):
         """Converts Seconds to a Human Readable Time String"""
         # Convert Seconds to Hours, Minutes, and Seconds
@@ -186,15 +208,17 @@ class BaseOrcaCalculation(ABC):
 
         # Return the Time String
         return ", ".join(parts) if parts else "0 seconds"
-    
-    def basisSetFunctionalCompliant (self):
+
+    def basisSetFunctionalCompliant(self):
         """Checks if the Basis Set and Functional Values have been defined. Used to simply call and confirm in Calculation Classes that require a Basis Set and Functional to be defined"""
-        if (not ("basis" in self.variables)):
+        if not ("basis" in self.variables):
             raise ValueError("BasisSet not defined! Provide Basis Set Name as a String")
-        
-        if (not ("functional" in self.variables)):
-            raise ValueError("Functional not defined! Provide Functional Name as a String")
-        
-    def setCalculationType (self, calculationType):
+
+        if not ("functional" in self.variables):
+            raise ValueError(
+                "Functional not defined! Provide Functional Name as a String"
+            )
+
+    def setCalculationType(self, calculationType):
         """Sets the Calculation Type in the Variables Dictionary"""
         self.variables["calculation"] = calculationType
